d8dd269c5362a8579f81fab5e5a88bcab0c2805c
Alex <licazago@fiec.espol.edu.ec>
2025-12-23 23:59:48 -0500

    Pruebas aledicaz

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,4 +1,3 @@
-# rails-api/config/routes.rb
 Rails.application.routes.draw do
   # Rutas API sin namespace :api, solo v1
   namespace :v1, path: 'api/v1' do
@@ -15,34 +14,34 @@ Rails.application.routes.draw do
         patch :toggle_status
       end
     end
+
     # Búsqueda Avanzada - Alex Otero
-      get 'search', to: 'search#index'
-      get 'search/suggestions', to: 'search#suggestions'
-      get 'search/categories_stats', to: 'search#categories_stats'
-      
-      # Favoritos - Alex Otero
-      resources :favorites, only: [:index, :create, :destroy] do
-        collection do
-          delete 'remove_by_listing/:listing_id', action: :remove_by_listing
-          get 'check/:listing_id', action: :check
-        end
+    get 'search', to: 'search#index'
+    get 'search/suggestions', to: 'search#suggestions'
+    get 'search/categories_stats', to: 'search#categories_stats'
+
+    # Favoritos - Alex Otero
+    resources :favorites, only: [:index, :create, :destroy] do
+      collection do
+        delete 'remove_by_listing/:listing_id', action: :remove_by_listing
+        get 'check/:listing_id', action: :check
       end
-      
-      # Usuarios y Perfiles - Alex Otero
-      resources :users, only: [] do
-        member do
-          get 'profile', action: :show
-          get 'listings'
-          get 'ratings'
-        end
-      end
-      
-      get 'users/me', to: 'users#me'
-      patch 'users/me', to: 'users#update_me'
-      
-      # Calificaciones
-      resources :ratings, only: [:create, :update, :destroy]
     end
+
+    # Usuarios y Perfiles - Alex Otero
+    resources :users, only: [] do
+      member do
+        get 'profile', action: :show
+        get 'listings'
+        get 'ratings'
+      end
+    end
+
+    get 'users/me', to: 'users#me'
+    patch 'users/me', to: 'users#update_me'
+
+    # Calificaciones
+    resources :ratings, only: [:create, :update, :destroy]
   end
   
   get 'health', to: 'health#check'
